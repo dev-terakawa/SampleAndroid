edd9618981a50c8fd16540d1719f63fd1be0bf93
dev-terakawa <dev.terakawa@gmail.com>
2020-03-09 18:36:24 +0900

    Update MainActivity.java

--- app/src/main/java/com/sample/android/MainActivity.java
+++ app/src/main/java/com/sample/android/MainActivity.java
@@ -3,11 +3,13 @@ package com.sample.android;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+//TODO
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //FIXME
         setContentView(R.layout.activity_main);
     }
 }
